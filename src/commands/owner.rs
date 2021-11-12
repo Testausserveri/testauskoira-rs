34d5d7c2a9f6f15ae79b1a7c1effeb56242dd1c3
DrVilepis <63553760+DrVilepis@users.noreply.github.com>
2021-11-13 00:21:07 +0200

    Removed redundant code

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -17,8 +17,6 @@ async fn quit(ctx: &Context, msg: &Message) -> CommandResult {
     } else {
         msg.reply(ctx, "There was a problem getting the shard manager")
             .await?;
-
-        return Ok(());
     }
 
     Ok(())
