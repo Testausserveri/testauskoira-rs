2ca70d9a084a3ef6766b6793c2467f343750a5f2
Luukas Pörtfors <lajp@iki.fi>
2022-01-02 04:15:51 +0200

    Get discord user profile pictures as PNGs
    This fixed a bug that caused the award ceremony not to work due to the
    lack of support for webp in the rust image libary.

--- src/utils/build_award.rs
+++ src/utils/build_award.rs
@@ -3,14 +3,14 @@ extern crate photon_rs;
 use photon_rs::native::*;
 
 pub async fn build_award_image(user_img_url: &str) -> Result<String, ()> {
-    let profile_picture = reqwest::get(format!("{}?size=128", user_img_url))
+    let img_url_base = &user_img_url[..user_img_url.rfind('.').unwrap()];
+    let profile_picture = reqwest::get(format!("{}.png?size=128", img_url_base))
         .await
         .unwrap()
         .bytes()
         .await
         .unwrap();
 
-    //FIXME open_image_from_bytes causes monochrome image, webp bad, fuck.
     let pfp_photon =
         open_image_from_bytes(profile_picture.as_ref()).expect("The profile-pic should be open");
     let mask_photon = open_image("img/blackcomposite.png").expect("mask.png should be open");
