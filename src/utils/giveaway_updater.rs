34e7f599e1c0da2c8670a3a867e56dfd4dc4a8a1
Mixu78 <hoppandebananen123@gmail.com>
2022-01-20 18:30:40 +0200

    Oopsie 2
    Missed more giveaway default env usages

--- src/utils/giveaway_updater.rs
+++ src/utils/giveaway_updater.rs
@@ -7,7 +7,7 @@ use crate::{commands::giveaway::end_giveaway, database::Database, Http, Reaction
 pub async fn update_giveaways(http: Arc<Http>, db: impl AsRef<Database>) {
     let db = db.as_ref();
     let reaction_emoji: char = std::env::var("GIVEAWAY_REACTION_EMOJI")
-        .expect("GIVEAWAY_REACTION_EMOJI is not set")
+        .unwrap_or("🎉".to_string())
         .parse()
         .expect("GIVEAWAY_REACTION_EMOJI is not a valid emoji");
     for g in db.get_ongoing_giveaways().await.unwrap().iter() {
